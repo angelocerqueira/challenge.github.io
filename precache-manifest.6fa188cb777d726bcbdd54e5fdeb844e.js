5abc59eecbb6096d186c6c2bf89b77d4dddffa10
Angelo Cerqueira <angelosc1998@gmail.com>
2020-06-18 20:57:11 -0400

    First Commit

---
+++ precache-manifest.6fa188cb777d726bcbdd54e5fdeb844e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49fee32ea173ce9f7a834b5f83d12ea8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0dd5d8576141e49f67df",
+    "url": "/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "0dd5d8576141e49f67df",
+    "url": "/static/js/2.7d215b80.chunk.js"
+  },
+  {
+    "revision": "d0550c944a9928585a31b6efc1a5d942",
+    "url": "/static/js/2.7d215b80.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d84ae555c543a1848e95",
+    "url": "/static/js/main.0314d72a.chunk.js"
+  },
+  {
+    "revision": "73c0105c35489003bd27",
+    "url": "/static/js/runtime-main.fb2aa805.js"
+  }
+]);
